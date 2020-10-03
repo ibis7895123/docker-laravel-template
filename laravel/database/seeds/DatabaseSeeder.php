b122daf9ec5a986c803f8a9c77b487c70bd31d8a
Toki Horii <toki.h@x-point-1.net>
2020-10-03 10:56:18 +0000

    first commit

---
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // $this->call(UserSeeder::class);
+    }
+}
