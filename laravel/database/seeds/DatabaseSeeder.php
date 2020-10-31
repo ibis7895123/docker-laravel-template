65cc9deb221a4a348a9e544cb0a557e9961c320d
Toki Horii <toki.h@x-point-1.net>
2020-10-31 18:31:09 +0900

    factoryの作成

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -9,6 +9,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(UsersTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            ContactFormsTableSeeder::class
+        ]);
     }
 }
