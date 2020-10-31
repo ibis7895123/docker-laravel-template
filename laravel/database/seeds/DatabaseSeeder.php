2faad1857c1a391c672858d4c968b08604c0b83d
Toki Horii <toki.h@x-point-1.net>
2020-10-31 18:15:49 +0900

    seeder追加

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -9,6 +9,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
